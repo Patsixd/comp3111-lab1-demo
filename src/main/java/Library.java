859a01bd800ab4b1e4638eab08b7eab4aa7bbd60
priscillachang <priscillachang@10.89.39.97>
2018-09-13 16:32:37 +0800

    I made a change

--- src/main/java/Library.java
+++ src/main/java/Library.java
@@ -3,6 +3,7 @@
  */
 public class Library {
 	public static void main(String arg[]) {
+		if (lib.someLibraryMethod());
 		System.out.println("When there is a fire, commit and push.");
 	}
     public boolean someLibraryMethod() {
